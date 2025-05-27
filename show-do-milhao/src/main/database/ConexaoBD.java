95d0627ab23c2e268c92139b3a81b578018cc81e
Guilherme Pereira <guilherme.pereira.ismart@gmail.com>
2025-05-26 21:19:24 -0300

    Update ConexaoBD.java

--- show-do-milhao/src/main/database/ConexaoBD.java
+++ show-do-milhao/src/main/database/ConexaoBD.java
@@ -1,1 +1,40 @@
 //conexão com o bando de dados
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+import java.io.FileInputStream;
+
+public class ConnectionFactory {
+    private String host;
+    private String port;
+    private String db;
+    private String user;
+    private String password;
+
+    public ConnectionFactory() throws Exception {
+        Properties props = new Properties();
+        props.load(new FileInputStream("config.properties"));
+
+        this.host = props.getProperty("db.host");
+        this.port = props.getProperty("db.port");
+        this.db = props.getProperty("db.name");
+        this.user = props.getProperty("db.user");
+        this.password = props.getProperty("db.password");
+    }
+
+    public Connection obterConexao() throws Exception {
+        var s = String.format("jdbc:mysql://%s:%s/%s", host, port, db);
+        Connection c = DriverManager.getConnection(s, user, password);
+        return c;
+    }
+
+    public static void main(String[] args) throws Exception {
+        var fabricaDeConexoes = new ConnectionFactory();
+        Connection conexao = fabricaDeConexoes.obterConexao();
+        if (conexao != null) {
+            System.out.println("Conectou!");
+        } else {
+            System.out.println("Não Conectou");
+        }
+    }
+}
